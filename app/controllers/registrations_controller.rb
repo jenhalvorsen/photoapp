b6d8abc5002a9ea6cf40318ef457233e74c563d0
jenhalvorsen <jensrh@online.no>
2020-02-07 21:33:37 +0100

    hei

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,13 @@
-class RegistrationsControler < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
   def create
-    build_resource(sign_upt_params)
+    build_resource(sign_up_params)
 
     resource.class.transaction do
       resource.save
       yield resource if block_given?
       if resource.persisted?
-        @payment = Payment.new({email: params["user"]["email"], token: params[:payment]["token"], user_id: resource.id })
+        @payment = Payment.new({email: params["user"]["email"],
+        token: params[:payment]["token"], user_id: resource.id})
 
         flash[:error] = "Please check registration errors" unless @payment.valid?
 
@@ -23,13 +24,15 @@ class RegistrationsControler < Devise::RegistrationsController
 
 
         if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
+          set_flash_message :notice, :signed_up if is_flashing_format?
           sign_up(resource_name, resource)
-          respond_with resource, location: after_sign_up_path_for(resource)
+          respond_with resource, location:
+          after_sign_up_path_for(resource)
         else
           set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
           expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+          respond_with resource, location:
+          after_inactive_sign_up_path_for(resource)
         end
       else
         clean_up_passwords resource
